735710ceb21e99db2dd2044357f083483d04d07f
Mansjam00 <jwj58929@gmail.com>
2024-09-13 11:20:20 +0900

    문제풀이

---
+++ 240909/exam.c
@@ -0,0 +1,27 @@
+// 백준 문제 번호: 18108
+// 문제 설명: 불기 연도를 서기 연도로 변환한 결과를 출력하기
+
+// #include <stdio.h>
+// int main(void)
+// {
+//     int a; // 변수 받기
+//     scanf("%d\n", &a); //변수 입력
+//     printf("%d\n", a-543); //불기연도를 서기연도로 출력
+//     return 0;
+// }
+
+// 백준 문제 번호: 10869
+// 문제 설명: 첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력
+
+// #include <stdio.h>
+// int main(void)
+// {
+//     int a,b; //변수 받기
+//     scanf("%d %d",&a,&b); //변수 입력
+//     printf("%d\n",a+b); //a+b 출력
+//     printf("%d\n",a-b); //a-b 출력
+//     printf("%d\n",a*b); //a*b 출력
+//     printf("%d\n",a/b); //a/b 출력
+//     printf("%d\n",a%b); //a%b 출력
+//     return 0;
+// }
