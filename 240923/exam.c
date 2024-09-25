95b36cf68bdd6a083acbc407bb96013e3a0387c4
Mansjam00 <jwj58929@gmail.com>
2024-09-25 12:30:24 +0900

    backjoon 문제풀이

---
+++ 240923/exam.c
@@ -0,0 +1,26 @@
+// 백준 문제 번호: 1330
+// 문제 설명: 두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.
+
+// #include <stdio.h>
+// int main(void){
+//     int a,b;
+
+//     scanf("%d %d", &a, &b);
+    
+//     if (a>b)
+//     {
+//         printf(">\n");
+//     }
+
+//     else if (a<b)
+//     {
+//         printf("<\n");
+//     }
+//     else
+//     {
+//         printf("==\n");
+//     }
+
+//     return 0;
+// }
+
