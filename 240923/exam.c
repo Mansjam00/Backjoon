75805020a97f70cea1fb48fb8d2d8196a3fdfde4
Mansjam00 <jwj58929@gmail.com>
2024-09-26 12:03:06 +0900

    Backjoon 문제 풀이

--- 240923/exam.c
+++ 240923/exam.c
@@ -24,3 +24,33 @@
 //     return 0;
 // }
 
+// 백준 문제 번호: 2753
+// 문제 설명: 윤년 계산 
+//
+// #include <stdio.h>
+
+// int main() {
+//     int a;
+//     scanf("%d", &a);
+//     printf("%d\n", ((a % 4 == 0 && a % 100 != 0) || (a % 400 == 0)) ? 1 : 0); 
+//   	return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(void){
+//     int a;
+//     scanf("%d", &a);
+//     if((a % 4 == 0 && a % 100 != 0) || (a % 400 == 0)){
+//         printf("1");
+//     }
+
+//     else{
+//         printf("0");
+//     }
+
+//     return 0;
+// }
+
+// 백준 문제 번호: 2753
+// 문제 설명: 윤년 계산 
