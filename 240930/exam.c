eb87ddb285e169b05563d559930b932ec77355fe
Mansjam00 <jwj58929@gmail.com>
2024-10-08 17:47:54 +0900

    baekjoon 문제풀이

--- 240930/exam.c
+++ 240930/exam.c
@@ -25,11 +25,38 @@
 // 백준 문제 번호: 2444
 // 문제 설명: 별 찍기
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int main(void){
-    int num;
-    int i, j;
+// int main(void)
+// {// 피라미드 출력
+//     int num;
+//     scanf("%d",&num);
+//     for(int i=0; i<num; i++)
+//     {
+//         for(int k=i; k<(num-1); k++)
+//         {
+//             printf(" ");
+//         }
+//         for(int k=0; k<=(i*2); k++)
+//         {
+//             printf("*");
+//         }
+//         printf("\n");
 
-    scanf("")
-}
+//     }
+//     // 역 피라미드 출력
+//     for(int i=(num-2); i>=0; i--)
+//     {
+//         for(int k=i; k<(num-1); k++)
+//         {
+//             printf(" ");
+//         }
+//         for(int k=0; k<=(i*2); k++)
+//         {
+//             printf("*");
+//         }
+//         printf("\n");
+
+//     }
+//     return 0;
+// }
