c95614a3f6582e2cb759f8a6569811267e69ca0d
Mansjam00 <jwj58929@gmail.com>
2024-10-10 19:22:40 +0900

    Baekjoon 문제풀이

---
+++ 241008/exam.c
@@ -0,0 +1,13 @@
+// 문제 설명 : 구구단
+
+// #include <stdio.h>
+
+// int main(void){
+//     for(int i = 1; i <= 9; i++){// 1에서 9까지 곱하는수 
+//         for(int j = 2; j <= 9; j++){ // 2단에서 9단까지 수
+//             printf("%d * %d = %d\t", j, i, j*i); // 출력
+//         }
+//         printf("\n"); //개행
+//     }
+//     return 0;
+// }
